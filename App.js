ef75f109dc556f32d51d17a3b8c8b47fb19a060d
Pohhong <pohhong.teng@pactmail.com>
2021-04-26 19:04:30 +0800

    Update UI and Refer file

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaViewComponent, StyleSheet,Text,View}from "react-native"
+import {StyleSheet,Text,View}from "react-native"
 
 export default class App extends React.Component{
   state = {
@@ -21,12 +21,135 @@ export default class App extends React.Component{
     }else {this.setState({botChoice:"Scissors"})}
   }
 
-  render(){
+  render() {
     return(
-    <View >
-      <Text>hello</Text>
+    <View style={styles.container}> 
+      <Text style={styles.Winner}>
+        Winner: 
+      </Text>
+      <View style={styles.gameContainer}>
+        <View style={styles.playerContainer}>
+          <Text style={styles.player}>
+            Player
+          </Text>
+          <Text style={styles.userscore}>
+            Score :
+          </Text>
+
+          <View style={styles.gameView}>
+            <View style={styles.winBackground}>
+              <Text style={styles.userChoice}>
+                Choice:
+              </Text>
+            </View>
+            
+          </View>
+
+        </View>
+      </View>
     </View>
     )
   }
 
 }
+const styles = StyleSheet.create({
+  container:{
+    flex : 1,
+    backgroundColor:'#000',
+    alignItems:"center",
+    justifyContent:"center",
+    flexDirection:"row"
+  },
+  gameContainer:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  playerContainer:{
+    margin:10,
+    borderWidth:10,
+    borderColor:"#fff",
+    padding: 20,
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  bot:{
+    margin:10,
+    borderColor:"#fff",
+    borderWidth:10,
+    padding:20,
+    alignItems :"center",
+    justifyContent:"center"
+  },
+  gameView:{
+    padding:20
+  },
+  winBackground:{
+    padding:20,
+    justifyContent:"center",
+    alignContent:"center",
+    alignItems:"center",
+    backgroundColor:"#fff"
+  },
+  choiceContainer:{
+    paddingVertical:20,
+    flexDirection:'row',
+    justifyContent:"space-evenly",
+    alignItems:"center",
+    margin:10
+  },
+  modalView:{
+    margin:20,
+    backgroundColor:"#fff",
+    borderRadius:20,
+    padding:35,
+    alignItems:"center",
+    shadowColor:"#fff",
+    shadowOffset:{width:0,height:2},
+    shadowOpacity:0.25,
+    shadowRadius:3.84,
+    elevation:5
+  },
+  openButton:{
+    backgroundColor:"#F194FF",
+    borderRadius:20,
+    padding:10,
+    elevation:2
+},
+textStyle:{
+  color:"#fff",
+  fontWeight:"bold",
+  textAlign:"center"
+},
+modalText:{
+  marginBottom:15,
+  textAlign:"center",
+  color:"#fff"
+},
+centeredView:{
+  justifyContent:"center",
+  alignItems:"center",
+  marginTop:22,
+  backgroundColor:"#fff"
+},
+Winner :{
+  color: "#fff",
+  fontWeight: "900",
+  fontSize: 30 
+},
+player:{
+  color:"#fff",
+  fontWeight:"900",
+  fontSize:25
+},
+userscore:{
+  color:"#fff",
+  fontWeight:"900",
+  fontSize:25
+},
+userChoice:{
+  color: "#000",
+  fontWeight: "900",
+  fontSize: 20
+}
+});
